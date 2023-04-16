d80baef6725a16e61cb0a48aebe043cddf861611
Eugene Orlov <unimws@gmail.com>
2023-04-16 23:48:01 +0300

    ДЗ-29 Задача выполнена

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,40 @@
+package ru.netology;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    private final static String URL =
+            "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+
+    public static void main(String[] args) {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+        try (CloseableHttpResponse response = httpClient.execute(new HttpGet(URL))) {
+            ObjectMapper mapper = new ObjectMapper();
+            List<CatFacts> facts = mapper.readValue(response.getEntity().getContent(),
+                    new TypeReference<List<CatFacts>>() {
+                    });
+
+            facts.stream()
+                    .filter(fact -> fact.getUpvotes() > 0)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
